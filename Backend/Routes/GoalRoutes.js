5359ce9180746e4dbf1c2a828fc13cf85d96857d
Henrik <henrikhenriksson87@gmail.com>
2024-06-11 10:42:19 +0200

    added asyncHandler dependency

--- Backend/Routes/GoalRoutes.js
+++ Backend/Routes/GoalRoutes.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { getGoals } = require("../Controllers/goalController");
+const {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+} = require("../Controllers/goalController");
 
-router.get("/", getGoals);
 
-router.post("/", (req, res) => {
-  res.status(200).json({ message: "Set goal" });
-});
-router.put("/:id", (req, res) => {
-  res.status(200).json({ message: `Update goal ${req.params.id}` });
-});
-router.delete("/:id", (req, res) => {
-  res.status(200).json({ message: `Delete goal ${req.params.id}` });
-});
+// Crud route calling
+router.route("/").get(getGoals).post(setGoal);
+router.route("/:id").delete(deleteGoal).put(updateGoal);
+
 
 module.exports = router;
