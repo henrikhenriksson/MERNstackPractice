90d9cb615c3c11701516e393830ae0f44a547a12
Henrik <henrikhenriksson87@gmail.com>
2024-06-17 14:36:23 +0200

    Integrated authentication middleware

---
+++ Backend/Middleware/authMiddleWare.js
@@ -0,0 +1,35 @@
+const expressAsyncHandler = require("express-async-handler");
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+const User = require("../Models/UserModel");
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      // get token from header
+      token = req.headers.authorization.split(' ')[1]; // split into array by the blankspace, get the token in index 1 of the resulting array
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      //  get user from the token:
+      req.user = await User.findById(decoded.id).select("-password"); // dont include the password
+      next(); // call the next part of the middleware
+    } catch (error) {
+      console.log(error);
+      res.status(401);
+      throw new Error("Not Authorized");
+    }
+  }
+
+  if (!token) {
+    res.status(402);
+    throw new Error("Not Authorized, no token");
+  }
+});
+
+module.exports = { protect };
--- Backend/Routes/UserRoutes.js
+++ Backend/Routes/UserRoutes.js
@@ -1,11 +1,16 @@
 // register // login // get user information
-
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser } = require("../Controllers/userController");
+const {
+  registerUser,
+  loginUser,
+  getMe,
+} = require("../Controllers/userController");
+
+const { protect } = require("../Middleware/authMiddleWare");
 
 router.post("/", registerUser);
 router.post("/login", loginUser);
-router.get("/me", loginUser);
+router.get("/me", protect, getMe); // call protect method on route.
 
 module.exports = router;
